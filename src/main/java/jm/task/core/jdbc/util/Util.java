0dfbe767deae897d8110a7545519a11753d6e7cb
Florexiz <florexiz.nm@gmail.com>
2021-08-14 01:34:24 +0300

    Implemented Hibernate connection

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,9 +1,18 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.Properties;
 
 public class Util {
+    private static SessionFactory sessionFactory;
+
     public static Connection getConnection() {
         Connection connection = null;
         try {
@@ -18,5 +27,22 @@ public class Util {
             e.printStackTrace();
         }
         return connection;
+    }
+
+    public static Session getSession() {
+        if (sessionFactory == null) {
+            Properties properties = new Properties();
+            properties.setProperty(Environment.URL, "jdbc:mysql://localhost:3306/manualtest");
+            properties.setProperty(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+            properties.setProperty(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
+            properties.setProperty(Environment.USER, "root");
+            properties.setProperty(Environment.PASS, "root");
+
+            sessionFactory = new Configuration()
+                    .addAnnotatedClass(User.class)
+                    .addProperties(properties)
+                    .buildSessionFactory();
+        }
+        return sessionFactory.openSession();
     }
 }
