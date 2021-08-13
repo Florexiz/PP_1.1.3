481c64b784e950731af8ab0b2818dfa664126c2a
Florexiz <florexiz.nm@gmail.com>
2021-08-14 01:34:24 +0300

    Implemented Hibernate connection

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,7 +1,12 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
 
+import javax.persistence.criteria.CriteriaQuery;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
@@ -12,31 +17,63 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
-
+        try (Session session = Util.getSession()) {
+            session.beginTransaction();
+            session.createSQLQuery(
+                    "CREATE TABLE IF NOT EXISTS user(" +
+                            "id INT PRIMARY KEY AUTO_INCREMENT," +
+                            "name VARCHAR(64)," +
+                            "lastName VARCHAR(64)," +
+                            "age INT" +
+                            ");"
+            ).executeUpdate();
+            session.getTransaction().commit();
+        }
     }
 
     @Override
     public void dropUsersTable() {
-
+        try (Session session = Util.getSession()) {
+            session.beginTransaction();
+            session.createSQLQuery(
+                    "DROP TABLE IF EXISTS user"
+            ).executeUpdate();
+            session.getTransaction().commit();
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        try (Session session = Util.getSession()) {
+            session.beginTransaction();
+            session.save(new User(name, lastName, age));
+            session.getTransaction().commit();
+            System.out.println("User with name " + name + " was added.");
+        }
     }
 
     @Override
     public void removeUserById(long id) {
-
+        try (Session session = Util.getSession()) {
+            session.beginTransaction();
+            session.createQuery("DELETE FROM User WHERE id = :id").setParameter("id", id).executeUpdate();
+            session.getTransaction().commit();
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        try (Session session = Util.getSession()) {
+            return session.createQuery("FROM User").list();
+        }
     }
 
     @Override
     public void cleanUsersTable() {
-
+        try (Session session = Util.getSession()) {
+            session.beginTransaction();
+            session.createQuery("DELETE FROM User").executeUpdate();
+            session.getTransaction().commit();
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,15 +1,41 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
+    private static SessionFactory sessionFactory;
+
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(
                 "jdbc:mysql://localhost:3306/manualtest?useSSL=false",
                 "root",
                 "root"
         );
+    }
+
+    public static Session getSession() {
+        if (sessionFactory == null) {
+            Properties properties = new Properties();
+            properties.setProperty(Environment.URL, "jdbc:mysql://localhost:3306/manualtest");
+            properties.setProperty(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+            properties.setProperty(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
+            properties.setProperty(Environment.USER, "root");
+            properties.setProperty(Environment.PASS, "root");
+
+            sessionFactory = new Configuration()
+                    .addAnnotatedClass(User.class)
+                    .addProperties(properties)
+                    .buildSessionFactory();
+        }
+        return sessionFactory.openSession();
     }
 }
